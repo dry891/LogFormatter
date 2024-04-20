f6126ef2c2e2fd671cfef4e49d3408f715ff130f
dry891 <namriairman@gmail.com>
2024-04-21 05:53:49 +0900

    最頻値関数の実装

--- main.js
+++ main.js
@@ -7,12 +7,14 @@ fileInput.addEventListener('change',  async (event) => {
     if (!file) return;
     await readHTML(file);// HTMLファイルを読み込む
     readTitle(file);// タイトルをファイル名から取得
-    chatlog = readChatlog();// チャットログを配列化
-    getActorColor(chatlog);
+    const  chatlog = readChatlog();// チャットログを配列化
+    actorColor = getActorColor(chatlog);// 発言者とその色を取得
+    console.log(actorColor)
 });
 
 // ファイル読み込み処理
 function readHTML(input){
+    console.log('readHTML')
     return new Promise((resolve) => {
         const reader = new FileReader();
         reader.onload = function (e) {
@@ -31,6 +33,7 @@ function readHTML(input){
 
 // タイトルをファイル名から取得（[all]以下は削除する）
 function readTitle(input){
+    console.log('readTitle')
     let title = input.name.substring(0, input.name.indexOf(".html"));
     if (title.indexOf("[all]") >= 0) {
         title = title.substring(0, title.lastIndexOf("[all]"));
@@ -41,8 +44,9 @@ function readTitle(input){
 
 // チャットログを配列化
 function readChatlog(){
+    console.log('readChatlog')
     const p_elements = document.querySelectorAll('#areaWorkspace p');
-    let logs = new Array();
+    let logs = new Array;
     Array.from(p_elements).forEach(function(p_element){
         logs.push({
             colorCode : p_element.outerHTML.match(/#([\da-fA-F]{6}|[\da-fA-F]{3)/)[0],//RGBで抜き出す場合はgetComputedStyle(p_element).color.match(/\d+/g)
@@ -56,20 +60,24 @@ function readChatlog(){
 
 // 発言者毎の色を取得する
 function getActorColor(input){
-    let actorList = input.map(e => e['actor']);// actor一覧を取得
-    actorList = Array.from(new Set(actorList));// 重複削除
-    actorList.forEach((act) => {
-        let colorList = input.filter(e => e['actor'] === act).map(e => e['colorCode']);
-        console.log('-----')
-        console.log(colorList)
-    })
+    console.log('getActorColor')
+    let actorList = Array.from(new Set(input.map(e => e['actor'])));// actor一覧（重複無し）を取得
+    let output = {}
+    actorList.map(act => {output[act] = mode(input.filter(e => e['actor'] === act).map(e => e['colorCode']))});// 各actorのcolorCodeを一覧で取得し、その最頻値を返す
+    return output
 }
 
 // 最頻値を求める関数
 function mode(input){
-
-    return output = input.sort().reduce((accumulator, currentValue, i, arr) => {
-        console.log(accumulator, currentValue, i, arr)
-        return currentValue
+    let index = new Map
+    let countMax = 0
+    let output
+    input.map(value =>{
+        let count = index.get(value)
+        count ? count++ : count = 1;
+        index.set(value,count)
+        count >= countMax ? (output = value,countMax = count) : null ;// 後から来た方を残す
+        return value
     });
+    return output
 }
--- test.js
+++ test.js
@@ -1,13 +1,64 @@
-let arr = ["apple","apple","pine","pine","apple","melon","orange"]
+let arr = ["melon","apple","apple","pine","apple","melon","orange"]
 
-const c = (x, i, v) => (x[i] ? x[i].add(v) : x[i] = new Set(v), i);
-const result = arr.reduce(
+const c = (x, i, v) => (x[i] ? x[i].add(v) : x[i] = new Set([v]), i);
+/*
+const result = arr.sort().reduce(
   function (x, v, i) { 
-    console.log("index "+i)
-    console.log(x)
-    console.log(v)
+    //console.log("index "+i)
+    //console.log(x)
+    //console.log(v)
     return (this.set(v, c(x, (this.get(v) + 1 || 1), v)), x); 
   }.bind(new Map), []).pop();
 
 
 console.log(result);
+
+const res = arr.sort().reduce(
+  function(x,v){
+      return ((this.set(v, c(x, (this.get(v) + 1 || 1), v)),console.log(this)), x)
+  }.bind(new Map),[])
+console.log(res)
+*/
+
+console.log('------------test03--------------')
+const test03 = arr.sort().reduce(function(x,v,i){
+  console.log(x,v)
+  console.log(this.get(v))
+  console.log(x[this.get(v) + 1])
+  this.set(v, c(x, (this.get(v) + 1 || 1), v))
+  //console.log(this)
+  return x
+}.bind(new Map),[])
+console.log(test03)
+
+/*
+https://camellia.hatenablog.jp/entry/2023/11/21/JavaScript_%E8%87%AA%E7%94%B1%E7%A0%94%E7%A9%B6_-_%E6%9C%80%E9%A0%BB%E5%80%A4%E3%82%92%E6%B1%82%E3%82%81%E3%82%8B%E3%82%B3%E3%83%BC%E3%83%89
+cの意味
+・(条件)? (trueの場合):(falseの場合)
+⇒
+if(x[i]){
+  x[i].add[v]
+}else{
+  x[i] = new Set(v)
+}
+の操作を行った後、iを戻り値とする。
+
+this.setのthis ⇒　bind(new Map)で初期化されたMap
+this.get(v)のthis ⇒　↑
+this.get(v) + 1 || 1
+⇒　||　論理和（OR)　条件分岐の省略として、左側がTrueなら左、違うなら右を返す
+falseとなるのは下記７つ
+・0
+・-0
+・null
+・false
+・NaN
+・undefined
+・空文字列 ("")
+
+pop() メソッドは、配列から最後の要素を取り除き、その要素を返します。このメソッドは配列の長さを変化させます。
+配列.pop() ⇒　配列の最後の要素
+配列.pop()した後の配列　⇒　1個短くなった配列
+
+const result = arr.reduce(function (x, v) { return (this.set(v, c(x, (this.get(v) + 1 || 1), v)), x); }.bind(new Map), []).pop();
+*/
