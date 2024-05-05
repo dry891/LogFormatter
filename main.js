200b2d21730a87cdf6ac08e59723d00e6f19863e
dry891 <namriairman@gmail.com>
2024-05-06 02:37:17 +0900

    テンプレートへの代入着手

--- main.js
+++ main.js
@@ -1,29 +1,25 @@
 //https://dry891.github.io/LogFormatter/
+const elem_configActor = document.getElementById('areaTemplate_configActor');
 
-// 「発言者別設定」欄の初期状態をセット
-const elem_configActor = document.getElementById('areaTemplate_configActor')
-const temp_configActor = elem_configActor.content.cloneNode(true);
-document.getElementById('configActors').appendChild(temp_configActor)
-/*
-fetch("./templates/editor.html").then(r => r.text()).then(r => {
-    const text = r.replace('$test','GM')
-    const fullhtml = new DOMParser().parseFromString(text, "text/html");
-    const html = fullhtml.querySelector("#id_configActor")
-    console.log(html)
-    document.getElementById('configActors').appendChild(html)
-})
-*/
+window.onload = function() {
+    // 「発言者別設定」欄の初期状態をセット
+    const temp_configActor = elem_configActor.content.cloneNode(true);
+    document.getElementById('configActors').appendChild(temp_configActor)
+};
+
 /*  ----------------------------------------------------------------------------
     １．ファイル選択時の処理
 ----------------------------------------------------------------------------　*/
 const fileInput = document.querySelector("#fileUploader");
+let chatlog = [];// チャットログ格納場所
 fileInput.addEventListener('change',  async (event) => {
     const file = event.currentTarget.files[0];
     if (!file) return;
     await readHTML(file);// HTMLファイルを読み込む
     readTitle(file);// タイトルをファイル名から取得
-    const chatlog = readChatlog();// チャットログを配列化
+    chatlog = readChatlog();// チャットログを配列化
     const actorList = getActorList(chatlog);// 発言者とその色を取得し、idを付ける
+    chatlog = appendActorId(actorList,chatlog); // ログ配列にidを付ける
     settingEditor(actorList) // 設定欄を表示
 });
 
@@ -32,7 +28,7 @@ function readHTML(input){
     return new Promise((resolve) => {
         const reader = new FileReader();
         reader.onload = function (e) {
-            var content = e.target.result;
+            let content = e.target.result;
             // ログ本文(<body>～</body>)を抽出し、見えない作業用スペース（areaWorkspace）に配置
             content = content.substring(content.indexOf("<body>")+6, content.indexOf("</body>")-7);
             document.getElementById("areaWorkspace").innerHTML = content;
@@ -59,13 +55,13 @@ function readTitle(input){
 // チャットログを配列化
 function readChatlog(){
     const p_elements = document.querySelectorAll('#areaWorkspace p');
-    let logs = new Array;
+    let logs = [];
     Array.from(p_elements).forEach(function(p_element){
         logs.push({
             colorCode : p_element.outerHTML.match(/#([\da-fA-F]{6}|[\da-fA-F]{3)/)[0],//RGBで抜き出す場合はgetComputedStyle(p_element).color.match(/\d+/g)
-            tab : p_element.children[0].innerText.substring(1,p_element.children[0].innerText.length-1),
+            tab : p_element.children[0].innerText.substring(2,p_element.children[0].innerText.length-1),
             actor : p_element.children[1].innerText,
-            message : p_element.children[2].innerText
+            message : p_element.children[2].innerText.slice(9,-7)
         });
     });
     return logs;
@@ -74,10 +70,10 @@ function readChatlog(){
 // 発言者とその色を取得し、idを付ける
 function getActorList(input){
     let actorList = Array.from(new Set(input.map(e => e['actor'])));// actor一覧（重複無し）を取得
-    let output = new Array;
+    let output = [];
     actorList.reduce((acc,act) => {
         output.push({
-            number : acc,
+            id : acc,
             actor : act,
             colorCode : mode(input.filter(e => e['actor'] === act).map(e => e['colorCode']))
         })
@@ -91,34 +87,42 @@ function mode(input){
     let index = new Map
     let countMax = 0
     let output
-    input.map(value =>{
-        let count = index.get(value)
+    input.forEach(value =>{
+        let count = -~index.get(value)
         count ? count++ : count = 1;
-        index.set(value,count)
-        count >= countMax ? (output = value,countMax = count) : null ;// 後から来た方を残す
-        return value
+        index.set(value,count);
+        count >= countMax && (output = value,countMax = count);// 後から来た方を残す
     });
     return output
 }
 
+// 発言者idを付けたchatlogを返す
+function appendActorId(actorList,log){
+    return log.map(mes => {
+        mes.actId = actorList.find(v => v.actor === mes.actor).id
+        return mes
+    });
+}
+
 function settingEditor(input){
-    let areaElement = document.getElementById('configActors')
+    let areaElement = document.getElementById('configActors');
     areaElement.removeChild(areaElement.lastElementChild);
-    console.log(input)
-    input.reduce((acc,data) => {
+    input.forEach(data => {
         let clone = elem_configActor.content.cloneNode(true);
-        clone.querySelector(".inputActorName").setAttribute("value",data['actor'])
-        clone.querySelector(".inputActorName").setAttribute("placeholder",data['actor'])
-        clone.querySelector(".inputActorColor").setAttribute("value",data['colorCode'])
-        clone.querySelector(".inputActorColor").setAttribute("placeholder",data['colorCode'])
-        clone.querySelector(".inputColorPicker").setAttribute("value",data['colorCode'])
-        document.getElementById('configActors').appendChild(clone)
+        clone.querySelector(".configActor").setAttribute("id","configActorId_" + data['id']);
+        clone.querySelector(".inputActorName").setAttribute("value",data['actor']);
+        clone.querySelector(".inputActorName").setAttribute("placeholder",data['actor']);
+        clone.querySelector(".inputActorColor").setAttribute("value",data['colorCode']);
+        clone.querySelector(".inputActorColor").setAttribute("placeholder",data['colorCode']);
+        clone.querySelector(".inputColorPicker").setAttribute("value",data['colorCode']);
+        document.getElementById('configActors').appendChild(clone);
     },0)
 }
 
 /*  ----------------------------------------------------------------------------
-    ２．色設定変更時
+    ２．エディタ操作時の処理
 ----------------------------------------------------------------------------　*/
+// 色変更時
 function colorChange(element){
     let value = element.value;
     element.parentElement.children[0].value = value;
@@ -128,5 +132,64 @@ function colorChange(element){
 /*  ----------------------------------------------------------------------------
     ３．出力
 ----------------------------------------------------------------------------　*/
-function convert(){
+document.querySelector('#btnConvert').addEventListener('click', async (e) => {
+    const title = document.getElementById("titleName").value;
+    const settedActorList = readActorList();
+    const templates = await loadTemplates();
+    const chat = makeChat(chatlog,templates.posts);
+
+    const content = posts;
+    //const html = new DOMParser().parseFromString(content, "text/html");
+    download(title + "_整形後",content);
+});
+
+function download(filename, content){
+    const blob = new Blob([content], {type: 'text/html'}); // 文字列をblobに変換
+    const anchor = document.createElement('a'); // アンカー要素を生成
+    anchor.download = filename + '.html'; // ファイル名指定
+    anchor.href = URL.createObjectURL(blob); // 仮のURL生成してhrefに入れる
+    anchor.click(); // アンカー要素のクリックイベントを発火
+    URL.revokeObjectURL(anchor.href); // メモリを解放
+};
+
+function readActorList(){
+    const elems = document.getElementsByClassName("configActor")
+    let output = []
+    Array.from(elems).map(elem => {
+        output.push({
+            id : Number(elem.id.substring(elem.id.indexOf("_")+1)),
+            actor : elem.querySelector(".inputActorName").value,
+            colorCode : elem.querySelector(".inputActorColor").value
+        });
+    });
+    return output
+};
+
+// テンプレートを取得
+async function loadTemplates(){
+    return {
+        all : await fetch("./templates/template.html").then(r => r.text()),
+        posts : await fetch("./templates/post.html").then(r => r.text()),
+        style : await fetch("./templates/template.html").then(r => r.text())
+    }
 }
+
+/* ---------- 出力内容の編集 ---------- */
+function makeHTML(temp,title,chat,style){
+    return temp.replace("{{title}}",title)
+        .replace("{{chat}}",chat)
+}
+
+// 発言内容
+function makeChat(log,temp){
+    let output = ""
+    log.forEach(p => {
+        let text = temp.replace('{{tabclass}}',p.tab)
+        .replace('{{actorclass}}','actId_'+p.actId)
+        .replace('{{tabName}}',p.tab)
+        .replace('{{actorName}}',p.actor)
+        .replace('{{message}}',p.message);
+        output = output + text
+    });
+    return output
+};
